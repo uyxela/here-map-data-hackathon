a88b3fc23f029396b50a19151abf50066804c09e
Aryan Misra <aryanmisra4@gmail.com>
2020-12-04 21:19:36 -0500

    hm

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // Screens
-import LoginScreen from './pages/LoginScreen';
-// import ProfileScreen from './screens/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import * as firebase from 'firebase';
 
 //React Navigation Setup
@@ -17,120 +17,14 @@ const firebaseConfig = {
   measurementId: 'G-KFMV1L3S56',
 };
 
-if (!firebase.apps.length) {
+// if (!firebase.apps.length) {
    firebase.initializeApp(firebaseConfig);
-}
-// React Native Bottom Navigation
-// https://aboutreact.com/react-native-bottom-navigation/
+// }
+const MainNavigator = createSwitchNavigator({
+  Login: { screen: LoginScreen },
+  Profile: { screen: ProfileScreen },
+});
 
-import 'react-native-gesture-handler';
+const App = createAppContainer(MainNavigator);
 
-import * as React from 'react';
-
-import
- MaterialCommunityIcons
-from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import {
-  NavigationContainer
-} from '@react-navigation/native';
-import {
-  createStackNavigator
-} from '@react-navigation/stack';
-import {
-  createBottomTabNavigator
-} from '@react-navigation/bottom-tabs';
-
-import HomeScreen from './pages/HomeScreen';
-import DetailsScreen from './pages/DetailsScreen';
-import ProfileScreen from './pages/ProfileScreen';
-import SettingsScreen from './pages/SettingsScreen';
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeStack() {
-  return (
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: '#42f44b' },
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' }
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home Page' }}/>
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: 'Details Page' }} />
-      </Stack.Navigator>
-  );
-}
-
-function SettingsStack() {
-  return (
-    <Stack.Navigator
-      initialRouteName="Settings"
-      screenOptions={{
-        headerStyle: { backgroundColor: '#42f44b' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' }
-      }}>
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{ title: 'Setting Page' }}/>
-      <Stack.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{ title: 'Details Page' }}/>
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ title: 'Profile Page' }}/>
-    </Stack.Navigator>
-  );
-}
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Feed"
-        tabBarOptions={{
-          activeTintColor: '#42f44b',
-        }}>
-        <Tab.Screen
-          name="HomeStack"
-          component={HomeStack}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="home"
-                color={color}
-                size={size}
-              />
-            ),
-          }}  />
-        <Tab.Screen
-          name="SettingsStack"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="settings"
-                color={color}
-                size={size}
-              />
-            ),
-          }} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
 export default App;
