3d089f2fe7d4f443e10f4710c8173ba6716637bf
Aryan Misra <aryanmisra4@gmail.com>
2020-12-07 14:23:44 -0500

    commit

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -1,28 +1,128 @@
 import React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 export default function ExerciseScreen(props) {
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    console.log("fetching user data");
+    var ref = firebase.database().ref("users/");
+    ref.once("value").then(function (snapshot) {
+      var tempuserInfo = snapshot.child(props.navigation.getParam("id")).val();
+
+      if (tempuserInfo) {
+        setUserInfo(tempuserInfo);
+      } else {
+        AsyncStorage.getItem("userId").then((id) =>
+          setUserInfo(snapshot.child(id).val())
+        );
+      }
+    });
+  }, []);
+  if (userInfo) {
     return (
       <View style={styles.container}>
-        <Text> Exercise Screen </Text>
+        <Text style={styles.titleText}> Exercise Log </Text>
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-          Welcome, {props.navigation.getParam("username")}
+          {userInfo["firstName"]} {userInfo["lastName"]}
         </Text>
+        <Text style={{ fontSize: 15, marginTop: 10 }}>{userInfo["email"]}</Text>
         <Button
           title="Sign out"
           onPress={() => props.navigation.navigate("Login")}
         />
+        <View
+          style={{
+            flexDirection: "row",
+            height: 170,
+            paddingTop: 10,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "#082D0F",
+              flex: 0.5,
+              margin: 10,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: "bold", color: "white" }}>
+              Male
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: "#B2FFA9",
+              flex: 0.5,
+              margin: 10,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: "bold", color: "black" }}>
+              18 yrs
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            height: 170,
+            paddingTop: 10,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "#00A9A5",
+              flex: 0.5,
+              margin: 10,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: "bold", color: "black" }}>
+              5' 11"
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: "#1B4965",
+              flex: 0.5,
+              margin: 10,
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 25, fontWeight: "bold", color: "white" }}>
+              170 lbs
+            </Text>
+          </View>
+        </View>
       </View>
     );
-  
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text> Loading </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: 70,
     alignItems: "center",
-    justifyContent: "center"
-  }
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
 });
--- screens/PlaceScreen.js
+++ screens/PlaceScreen.js
@@ -1,90 +1,117 @@
-import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import * as firebase from "firebase";
+import MapView from "react-native-maps";
 import { Ionicons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 
 export default function WorkoutScreen(props) {
+  const [location, setLocation] = useState(null);
+  const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Workout Saved!",
+      "Boof some meth as a reward :)",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+  useEffect(() => {
+    console.log("fetching user data");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation(position);
+      },
+      (error) => Alert.alert(error.message),
+      {
+        enableHighAccuracy: false,
+        timeout: 20000,
+        maximumAge: Infinity,
+      }
+    );
+  }, []);
   return (
     <View style={styles.container}>
-        <Button title="Back" onPress={() => props.navigation.navigate("Workout")}/>
-      <Text style={styles.titleText}> Place Title </Text>
+      <Text style={styles.titleText}> Place Name </Text>
+
+      <Button
+        title="Back"
+        onPress={() => props.navigation.navigate("Workout")}
+      />
       <View
         style={{
           flexDirection: "row",
           height: 400,
-          paddingTop: 10,
+        }}
+      >
+        <View style={styles.mapView}>
+          {location ? (
+            <MapView
+              style={styles.mapStyle}
+              initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+              }}
+              showsUserLocation={true}
+              userLocationAnnotationTitle={"Current Location"}
+              showsMyLocationButton={true}
+              loadingEnabled={true}
+              showsPointsOfInterest={true}
+              showsMyLocationButton={true}
+            ></MapView>
+          ) : null}
+        </View>
+      </View>
+      <Text style={{ fontSize: 15 }}> 2 mi away </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          height: 50,
+          marginTop: 10,
         }}
       >
         <View
           style={{
-            backgroundColor: "#00A9A5",
-            flex: 0.75,
-            margin: 10,
-            borderRadius: 20,
+            flex: 0.5,
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Text style={{ fontSize: 25, fontWeight: "bold", color: "black" }}>
-            MAP GOES HERE
-          </Text>
+          <Text style={{ fontSize: 16, color: "black" }}>Turn left</Text>
+        </View>
+        <View
+          style={{
+            flex: 0.5,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ fontSize: 16, color: "black" }}>in 250 yd</Text>
         </View>
       </View>
       <View
         style={{
           flexDirection: "row",
           height: 50,
-          //   backgroundColor: "red",
           marginTop: 10,
         }}
       >
-        <View
-          style={{
-            flex: 0.4,
-            alignItems: "left",
-            justifyContent: "center",
-          }}
+        <TouchableOpacity
+          onPress={createTwoButtonAlert}
+          style={styles.buttonContainer}
         >
-          <Text style={{ fontSize: 16, color: "black" }}>Place Name</Text>
-        </View>
-        <View
-          style={{
-            flex: 0.4,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ fontSize: 16, color: "black" }}>0.7 mi</Text>
-        </View>
-        
-          <View
-            style={{
-              flex: 0.1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <AntDesign name="arrowright" size={24} color="black" />
+          <View style={{ alignContent: "left" }}>
+            <Text style={styles.buttonText}>Record Workout</Text>
           </View>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          height: 50,
-          marginTop: 10,
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Button title="Log Workout" />
-        </View>
-        
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -100,5 +127,40 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 40,
     fontWeight: "bold",
+  },
+  mapStyle: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 25,
+    borderWidth: 6,
+    borderStyle: "solid",
+    borderColor: "#00A9A5",
+  },
+  mapView: {
+    width: "80%",
+    height: "90%",
+    margin: 20,
+    backgroundColor: "#f7f7f7",
+    borderRadius: 25,
+  },
+  buttonContainer: {
+    width: "80%",
+    height: "100%",
+    padding: 10,
+    backgroundColor: "#00A9A5",
+    borderRadius: 15,
+    margin: "auto",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: "auto",
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "500",
+    color: "white",
+    textAlign: "center",
   },
 });
