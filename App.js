56f70f28a5997adb694dd07db4d2cf2be3f6c8d0
Aryan Misra <aryanmisra4@gmail.com>
2020-12-07 02:03:53 -0500

    Update App.js

--- App.js
+++ App.js
@@ -6,17 +6,8 @@ import {TabScreen} from "./screens/TabScreen";
 
 //React Navigation Setup
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import firebaseConfig from './firebaseConfig.json';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBQ1M4vxJTywuLidAuJ7e6xryQ2QZnK0X8",
-  authDomain: "quickstart-1586651695794.firebaseapp.com",
-  databaseURL: "https://quickstart-1586651695794-default-rtdb.firebaseio.com",
-  projectId: "quickstart-1586651695794",
-  storageBucket: "quickstart-1586651695794.appspot.com",
-  messagingSenderId: "161357227708",
-  appId: "1:161357227708:web:287cab380cdf9c7a8152b9",
-  measurementId: "G-KFMV1L3S56",
-};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
