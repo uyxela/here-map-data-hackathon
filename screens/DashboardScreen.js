91e086acef567d37d5c1db55dfeb20b6c82c66fe
Aryan Misra <aryanmisra4@gmail.com>
2020-12-07 13:42:04 -0500

    changes

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,52 +1,85 @@
 import React, { useState, useEffect } from "react";
-import { Alert, Text, View, StyleSheet, Button, Dimensions, TouchableOpacity  } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+
+import {
+  Alert,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 import * as firebase from "firebase";
 import MapView from "react-native-maps";
 
 export default function DashboardScreen(props) {
-
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      setLocation(position);
-    }, error => Alert.alert(error.message), { 
-      enableHighAccuracy: true, 
-      timeout: 20000, 
-      maximumAge: 1000 
+    console.log("fetching user data");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation(position);
+      },
+      (error) => Alert.alert(error.message),
+      {
+        enableHighAccuracy: false,
+        timeout: 20000,
+        maximumAge: Infinity,
+      }
+    );
+    var ref = firebase.database().ref("users/");
+    ref.once("value").then(function (snapshot) {
+      var tempuserInfo = snapshot.child(props.navigation.getParam("id")).val();
+
+      if (tempuserInfo) {
+        setUserInfo(tempuserInfo);
+      } else {
+        AsyncStorage.getItem("userId").then((id) =>
+          setUserInfo(snapshot.child(id).val())
+        );
+      }
     });
-
     // get user info
-    setUserInfo({firstName: "Alex"})
-  }, [])
+  }, []);
 
-  if (location && userInfo) {
+  if (userInfo) {
     return (
       <View style={styles.container}>
         <Text style={styles.heading1}>Welcome, {userInfo["firstName"]}</Text>
         <View style={styles.mapView}>
-          <MapView
-            style={styles.mapStyle}
-            initialRegion={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-              latitudeDelta: 0.1,
-              longitudeDelta: 0.1
-            }}
-            showsUserLocation={true}
-            userLocationAnnotationTitle={"Current Location"}
-            showsMyLocationButton={true}
-            loadingEnabled={true}
-            showsPointsOfInterest={true}
-            showsMyLocationButton={true}
-          >
-            <TouchableOpacity onPress={() => {}} style={styles.buttonContainer}>
-              <Text style={styles.buttonText}>Let's explore</Text>
-              <Ionicons name="ios-arrow-forward" color="white" size={28} />
-            </TouchableOpacity>
-          </MapView>
+          {location ? (
+            <MapView
+              style={styles.mapStyle}
+              initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+              }}
+              showsUserLocation={true}
+              userLocationAnnotationTitle={"Current Location"}
+              showsMyLocationButton={true}
+              loadingEnabled={true}
+              showsPointsOfInterest={true}
+              showsMyLocationButton={true}
+            >
+              <TouchableOpacity
+                onPress={() => props.navigation.navigate("Workout")}
+                style={styles.buttonContainer}
+              >
+                <View style={{ alignContent: "left" }}>
+                  <Text style={styles.buttonText}>Let's explore</Text>
+                </View>
+                <View style={{ alignContent: "left" }}>
+                  <Ionicons name="ios-arrow-forward" color="white" size={28} />
+                </View>
+              </TouchableOpacity>
+            </MapView>
+          ) : null}
         </View>
         <Text style={styles.heading2}>Your Weekly Stats</Text>
         <View style={styles.statsContainer}>
@@ -70,7 +103,6 @@ export default function DashboardScreen(props) {
       </View>
     );
   }
-  
 }
 
 const styles = StyleSheet.create({
@@ -78,7 +110,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 70,
+    // justifyContent: "center",
   },
   mapStyle: {
     width: "100%",
@@ -86,23 +119,25 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     borderWidth: 6,
     borderStyle: "solid",
-    borderColor: "#00A9A5"
+    borderColor: "#00A9A5",
   },
   mapView: {
     width: "80%",
     height: "35%",
-    margin: 20
+    margin: 20,
+    backgroundColor: "#f7f7f7",
+    borderRadius: 25,
   },
   heading1: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: "bold",
     width: "80%",
-    marginTop: "25%"
+    // marginTop: "25%",
   },
   heading2: {
     fontSize: 28,
     width: "80%",
-    marginTop: 20
+    marginTop: 20,
   },
   buttonContainer: {
     width: "90%",
@@ -115,15 +150,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignSelf: "center",
     marginTop: "auto",
-    marginBottom: 10
+    marginBottom: 10,
   },
   buttonText: {
-    fontSize: 28,
+    fontSize: 24,
     fontWeight: "500",
     color: "white",
-    textAlign: "center"
+    textAlign: "center",
   },
-  statsContainer : {
+  statsContainer: {
     width: "80%",
     height: "30%",
     margin: 20,
@@ -131,24 +166,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#1B4965",
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems: "center"
-  }, 
+    alignItems: "center",
+  },
   statsGroup: {
     width: "30%",
     height: "80%",
     flexDirection: "column",
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
   },
   labelGroup: {
     width: "50%",
     height: "80%",
     flexDirection: "column",
-    justifyContent: "space-around"
+    justifyContent: "space-around",
   },
   statsText: {
     color: "white",
     fontSize: 20,
-    fontWeight: "500"
-  }
+    fontWeight: "500",
+  },
 });
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import * as Google from "expo-google-app-auth";
 import * as firebase from "firebase";
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from "@react-native-community/async-storage";
 
 const IOS_CLIENT_ID =
   "161357227708-bsnervueaobkrj0ffk1egodng4hmtoef.apps.googleusercontent.com";
@@ -25,32 +25,31 @@ export default function LoginScreen(props) {
           if (snapshot.child(result.user.id).exists()) {
             props.navigation.navigate("Profile", {
               id: result.user.id,
-            }); 
-            AsyncStorage.setItem('userId',result.user.id);
-          }
-          else{
-            firebase
-            .database()
-            .ref("users/" + result.user.id)
-            .set({
-              email: result.user.email,
-              firstName: result.user.givenName,
-              lastName: result.user.familyName,
-              id: result.user.id,
-              photoUrl: result.user.photoUrl,
-              gender: null,
-              age: null,
-              height: null,
-              weight: null,
             });
+            AsyncStorage.setItem("userId", result.user.id);
+          } else {
+            firebase
+              .database()
+              .ref("users/" + result.user.id)
+              .set({
+                email: result.user.email,
+                firstName: result.user.givenName,
+                lastName: result.user.familyName,
+                id: result.user.id,
+                photoUrl: result.user.photoUrl,
+                gender: null,
+                age: null,
+                height: null,
+                weight: null,
+              });
             props.navigation.navigate("Profile", {
               id: result.user.id,
-            }); 
-            AsyncStorage.setItem('userId',result.user.id);
+            });
+            AsyncStorage.setItem("userId", result.user.id);
           }
+          
         });
 
-        
         return result.accessToken;
       } else {
         return { cancelled: true };
