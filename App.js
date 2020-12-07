1c035e6ee87ef3c488360b77bda8a2be547b8972
Alex Yu <alexjy@yahoo.com>
2020-12-07 00:04:37 -0500

    Add dashboard and exercise screens, refactor firebase credentials

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
 // Screens
 import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import LoggingScreen from './screens/LoggingScreen';
 import ProfileScreen from './screens/ProfileScreen';
+
+// Firebase
 import * as firebase from 'firebase';
+import "firebaseConfig.json"
 
 //React Navigation Setup
 import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBQ1M4vxJTywuLidAuJ7e6xryQ2QZnK0X8',
-  authDomain: 'quickstart-1586651695794.firebaseapp.com',
-  databaseURL: 'https://quickstart-1586651695794-default-rtdb.firebaseio.com',
-  projectId: 'quickstart-1586651695794',
-  storageBucket: 'quickstart-1586651695794.appspot.com',
-  messagingSenderId: '161357227708',
-  appId: '1:161357227708:web:287cab380cdf9c7a8152b9',
-  measurementId: 'G-KFMV1L3S56',
-};
-
 // if (!firebase.apps.length) {
    firebase.initializeApp(firebaseConfig);
 // }
+
 const MainNavigator = createSwitchNavigator({
   Login: { screen: LoginScreen },
+  Dashboard: { screen: DashboardScreen },
+  Logging: { screen: LoggingScreen },
   Profile: { screen: ProfileScreen },
 });
 
