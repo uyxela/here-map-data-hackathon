3daa6ac0f9b6f9509dda1065cdebdb50e77ac4c6
Aryan Misra <aryanmisra4@gmail.com>
2020-12-07 01:58:49 -0500

    fucking navbar

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
 // Screens
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import * as firebase from 'firebase';
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import * as firebase from "firebase";
+import {TabScreen} from "./screens/TabScreen";
 
 //React Navigation Setup
-import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBQ1M4vxJTywuLidAuJ7e6xryQ2QZnK0X8',
-  authDomain: 'quickstart-1586651695794.firebaseapp.com',
-  databaseURL: 'https://quickstart-1586651695794-default-rtdb.firebaseio.com',
-  projectId: 'quickstart-1586651695794',
-  storageBucket: 'quickstart-1586651695794.appspot.com',
-  messagingSenderId: '161357227708',
-  appId: '1:161357227708:web:287cab380cdf9c7a8152b9',
-  measurementId: 'G-KFMV1L3S56',
+  apiKey: "AIzaSyBQ1M4vxJTywuLidAuJ7e6xryQ2QZnK0X8",
+  authDomain: "quickstart-1586651695794.firebaseapp.com",
+  databaseURL: "https://quickstart-1586651695794-default-rtdb.firebaseio.com",
+  projectId: "quickstart-1586651695794",
+  storageBucket: "quickstart-1586651695794.appspot.com",
+  messagingSenderId: "161357227708",
+  appId: "1:161357227708:web:287cab380cdf9c7a8152b9",
+  measurementId: "G-KFMV1L3S56",
 };
 
-// if (!firebase.apps.length) {
-   firebase.initializeApp(firebaseConfig);
-// }
-const MainNavigator = createSwitchNavigator({
-  Login: { screen: LoginScreen },
-  Profile: { screen: ProfileScreen },
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+const AppStack = createSwitchNavigator({ TabScreen });
+
+const MainNavigator = createSwitchNavigator(
+  {
+    Login: { screen: LoginScreen },
+    App: { screen: AppStack },
+  },
+);
 
 const App = createAppContainer(MainNavigator);
 
